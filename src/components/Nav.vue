b8213017da3dc480ffe7d8314b19159ae40fedd3
krystel <krystel.dev@gmail.com>
2022-12-10 02:54:19 +0100

    clean commit nav component

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -25,51 +25,6 @@ const props = defineProps({
   username: String,
 });
 
-// // Conseguir nombre del email:
-// const getUser = useUserStore().user;
-// const getUserEmail = getUser.email;
-// const indexArroba = getUser.email.indexOf("@", 1);
-// const getUSerNameEmail = getUserEmail.slice(0, indexArroba);
-
-// // Conseguir nombre real del usuario:
-// const userStore = useUserStore();
-
-// // Variable para guardar el perfil de supabase
-// const profile = ref({
-//   avatar_url: "",
-//   created_at: "",
-//   name: "",
-//   user_id: "",
-//   username: "",
-//   website: "",
-// });
-// const username = ref(null);
-// const avatar_url = ref(null);
-// const user_id = ref(null);
-
-// // PREFILE
-// const getProfile = async () => {
-//   // Hago una llamada para que no vaya más rapido que el primer fetchUser de App.vue
-//   await userStore.fetchUser();
-//   // Guardo el perfil en una constante:
-//   profile.value = userStore.profile;
-
-//   // Cuando se registra un user no tiene nombre.
-//   //Si ya ha editado el nombre vacio:
-//   if (profile.value.username) {
-//     username.value = profile.value.username;
-//   } else {
-//     // Si aun no ha registrado un nombre:
-//     const getUser = useUserStore().user;
-//     const getUserEmail = getUser.email;
-//     const indexArroba = getUser.email.indexOf("@", 1);
-//     username.value = getUserEmail.slice(0, indexArroba);
-//   }
-//   // avatar_url.value = profile.value.avatar_url;
-//   // user_id.value = profile.value.user_id;
-// };
-// getProfile();
-
 // async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
 const redirect = useRouter();
 
