04991d6aed37e6176e3ac0595c7be9a6250245fa
krystel <krystel.dev@gmail.com>
2022-12-12 12:13:33 +0100

    footer nav pegajoso mobile

---
+++ src/components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/tasks">Task Manager</router-link>
+    <div>
+      <ul>
+        <li>
+          <p>Welcome,</p>
+          <router-link to="/account">{{ props.username }}</router-link>
+        </li>
+        <li>
+          <button @click="signOut()">Log out</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useUserStore } from "../stores/user";
+
+const props = defineProps({
+  username: String,
+});
+// async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
+const redirect = useRouter();
+
+// Arrow function to Signout user to supaBase
+const signOut = async () => {
+  try {
+    // calls the user store and send the users info to backend to logOut
+    await useUserStore().signOut();
+    // redirects user to the singup
+    redirect.push({ path: "/auth/login" });
+  } catch (error) {
+    // displays error message
+    errorMsg.value = error.message;
+    // hides error message
+    setTimeout(() => {
+      errorMsg.value = null;
+    }, 5000);
+  }
+  return;
+};
+</script>
+
+<style>
+nav {
+  background-color: lightgray;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+}
+
+nav ul {
+  list-style: none;
+  padding-inline-start: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
